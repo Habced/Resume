6b42fe84bd7048bb20d3a12b73722946125f9047
Habced <seungyboy@gmail.com>
2020-08-05 15:20:07 +0900

    Added Algo Page

--- web/index.php
+++ web/index.php
@@ -22,6 +22,12 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
+
+$app->get('/algo', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('algo.twig');
+});
+
 // $app->get('/cowsay', function() use($app) {
 //   $app['monolog']->addDebug('cowsay');
 //   return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
