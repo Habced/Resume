d0956f44adaac1da0287d5dd09d5842accafa73a
Habced <seungyboy@gmail.com>
2023-10-05 17:36:38 +0900

    Stuff so far

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
     <!-- Font Awesome icons (free version)-->
     <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+		<!-- Devicon v2.15.1 -->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css">
     <!-- Google fonts-->
     <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
@@ -26,8 +28,8 @@
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav">
 					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
-					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
 					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
 					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#projects">Projects</a></li>
 					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
 					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
@@ -45,8 +47,8 @@
 						<span class="text-primary">Lee</span>
 					</h1>
 					<div class="subheading mb-5">
-						3542 Berry Street · Cheyenne Wells, CO 80810 · (317) 585-8468 ·
-						<a href="mailto:name@email.com">name@email.com</a>
+						+82 (010) 5842-9798 &nbsp;
+						<a href="mailto:haseung.lee.1997@gmail.com">haseung.lee.1997@gmail.com</a>
 					</div>
 					<p class="lead mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
 					<div class="social-icons">
@@ -58,26 +60,142 @@
 				</div>
 			</section>
 			<hr class="m-0" />
+			<!-- Experience-->
+			<section class="resume-section" id="experience">
+				<div class="resume-section-content">
+					<h2 class="mb-5">Experience</h2>
+					<div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+						<div class="flex-grow-1">
+							<h3 class="mb-0 flex-shrink-0">Senior Software Engineer</h3>
+							
+							<div class="subheading mb-0 text-primary"><a href="https://www.saramin.co.kr/zf_user/company-info/view/csn/ZCsvVzV6cE5hTUhiOEszT2ZRTHFuUT09/company_nm/(%EC%A3%BC)%EB%8D%94%EC%A2%8B%EC%9D%80%EC%9A%B4%EB%8F%99%EC%9C%BC%EB%A1%9C">(주)더좋은운동으로 The Better Exercise</a></div>
+							<div class="flex-shrink-0"><i class="fas fa-calendar-alt"></i>&nbsp;2019 February - 2022 June</div>
+							
+							<p>
+								<ul>
+									<li> Started as a part-time contractor and later became a full-time Senior Software Engineer. </li>
+									<li> Designed and built, <a href="https://tboxfit.com">T-Box Fit</a> , a responsive platform that gathered subscriptions and raised revenue. </li>
+									<li> Setup a live streaming studio for VR and 360 to create media content that is now monetized. <u>NDA</u> </li>
+									<li> Lead and attended business meetings with suppliers. The end product is now patented. <u>NDA</u> </li>
+									<li> Participated in meetings about software needs and limitations of PCBs and embedded software. </li>
+									<li> The platforms I built are now being used for B2G, B2B, and B2C services. <u>NDA</u> </li>
+									<li> Built several apps that were presented on and won grand prizes as well as significant funding. <a href="https://play.google.com/store/apps/details?id=tboxfit.app.tboxapp">eg</a> </li>
+									<ul style=“list-style-type:circle”>
+										<li> The most notable awards include... </li>
+										<ol>
+											<li> Venture Startup award from Incheon City Mayor. </li>
+											<li> The largest office at <a href="https://www.startuppark.kr/eng/main.do">Songdo Startup Park</a> for four years with all expenses paid.  </li>
+											<li> 2020 Next Media Competition Grand Prize.  </li>
+										</ol>
+									</ul>
+									<li> Worked with <a href="https://www.inu.ac.kr/mbshome/mbs/inu/index.do">Incheon National University</a> and <a href="https://www.daekyo.com/">Daekyo</a> to develop <a href="https://www.instagram.com/bangcl_official/">Bangcl</a>, an e-learning platform. </li>
+									<ul>
+										<li> The online classroom includes patented software. <u>NDA</u> </li>
+										<li> Managed communications between companies which resulted in increased efficiency.  </li>
+										<li> Managed communications with users for feedback which further improved the platform.  </li>
+										<li> Managed communications with instructors on best practices for online courses.  </li>
+										<li> The platform collected revenue from online registrations. <u>NDA</u> </li>
+									</ul>
+									<li> Mentored several junior engineers in modern software engineering practices. </li>
+									<li> Filtered through resumes and conducted interviews for new software developers. </li>
+									<li> The software development team increased from one to four people within the company. </li>
+									<li> Managed communications between the development team and stakeholders. </li>
+								</ul>
+								*Cannot share source code or precise details due to <u>NDA</u> 
+
+							</p>
+						</div>
+					</div>
+					
+					<div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+						<div class="flex-grow-1">
+							<h3 class="mb-0">Military Service - Squad Commander</h3>
+							<div class="subheading mb-0 text-primary">Republic Of Korea Army - The 5th Armored Brigade - 113 Mechanized Infantry Battalion</div>
+							<div class="flex-shrink-0"><i class="fas fa-calendar-alt"></i>&nbsp;2022 June - 2023 December</div>
+							<p>
+								<ul>
+									<li>Key responsibilities include:</li>
+									<ul>
+										<li><em>Leadership</em> - Directing the squad in the execution of their duties, training, operations, and administration.</li>
+										<li><em>Planning</em> - Developing operation plans and coordinating with other commanders.</li>
+										<li><em>Maintaining Discipline</em> - Maintaining self and squad discipline.</li>
+										<li><em>Ensuring Safety and Welfare</em> - Responsible for the safety and welfare of the squad; during training, operations, and recreational time. Making sure everyone on the squad in properly equipped and fed, but also addressing issues regarding morale, discipline, and actions.</li>
+										<li><em>Communcation and Coordination</em> - Making sure to communicate effectively with the squad as well as other commanders.</li>
+									</ul>
+									<li>Key acheivements include:</li>
+									<ul>
+										<li></li>
+									</ul>
+								</ul>
+							</p>
+
+						</div>
+					</div>
+				</div>
+			</section>
+			<hr class="m-0" />
 			<!-- Skills-->
 			<section class="resume-section" id="skills">
 				<div class="resume-section-content">
 					<h2 class="mb-5">Skills</h2>
-					<div class="subheading mb-3">Programming Languages & Tools</div>
+					<div class="subheading mb-3">Programming And Markup Languages</div>
+					
+					<!-- TODO add typescript, django, python, AWS, flutter, dart, java, android studio, kotlin, php, firebase, heroku, mysql, nosql, cleardb,  -->
+					<ul class="fa-ul mb-0">
+						<li><span class="fa-li"><i class="devicon-html5-plain"></i></span>HTML5</li>
+						<li><span class="fa-li"><i class="devicon-css3-plain"></i></span>css & scss</li>
+						<li><span class="fa-li"><i class="devicon-javascript-plain"></i></span>Javascript</li>
+						<li><span class="fa-li"><i class="devicon-typescript-plain"></i></span>Typescript</li>
+						<li><span class="fa-li"><i class="devicon-php-plain"></i></span>PHP</li>
+						<li><span class="fa-li"><i class="devicon-java-plain"></i></span>Java</li>
+						<li><span class="fa-li"><i class="devicon-kotlin-plain"></i></span>Kotlin</li>
+						<li><span class="fa-li"><i class="devicon-dart-plain"></i></span>Dart</li>
+						<li><span class="fa-li"><i class="devicon-python-plain"></i></span>Python</li>
+					</ul>
+					<!-- <div class="subheading mb-3">Programming Frameworks</div>
 					<ul class="list-inline dev-icons">
-						<li class="list-inline-item"><i class="fab fa-html5"></i></li>
-						<li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
-						<li class="list-inline-item"><i class="fab fa-js-square"></i></li>
 						<li class="list-inline-item"><i class="fab fa-angular"></i></li>
 						<li class="list-inline-item"><i class="fab fa-react"></i></li>
 						<li class="list-inline-item"><i class="fab fa-node-js"></i></li>
 						<li class="list-inline-item"><i class="fab fa-sass"></i></li>
 						<li class="list-inline-item"><i class="fab fa-less"></i></li>
-						<li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
-						<li class="list-inline-item"><i class="fab fa-gulp"></i></li>
-						<li class="list-inline-item"><i class="fab fa-grunt"></i></li>
 						<li class="list-inline-item"><i class="fab fa-npm"></i></li>
+					</ul> -->
+					<br>
+					<div class="subheading mb-3">Programming Frameworks</div>
+					<ul class="fa-ul mb-0">
+						<li><span class="fa-li"><i class="devicon-angularjs-plain"></i></span>Angular</li>
+						<li><span class="fa-li"><i class="devicon-django-plain"></i></span>Django & Django-Rest-Framework</li>
+						<li><span class="fa-li"><i class="devicon-react-plain"></i></span>React</li>
+						<li><span class="fa-li"><i class="devicon-flutter-plain"></i></span>Flutter</li>
 					</ul>
-					<div class="subheading mb-3">Workflow</div>
+					<br>
+					<div class="subheading mb-3">Databases</div>
+					<ul class="fa-ul mb-0">
+						<li><span class="fa-li"><i class="devicon-mysql-plain"></i></span>MySql</li>
+						<li><span class="fa-li"><i class="devicon-postgresql-plain"></i></span>PostgreSQL</li>
+						<li><span class="fa-li"><i class="devicon-sqlite-plain"></i></span>SQLite</li>
+					</ul>
+					<br>
+					<div class="subheading mb-3">Other Skills And Tools</div>
+
+					<ul class="fa-ul mb-0">
+						<li><span class="fa-li"><i class="devicon-git-plain"></i></span>Git & Github</li>
+						<li><span class="fa-li"><i class="devicon-amazonwebservices-plain"></i></span>AWS (architecture, ec2, s3)</li>
+						<li><span class="fa-li"><i class="devicon-linux-plain"></i></span>Linux</li>
+						<li><span class="fa-li"><i class="devicon-figma-plain"></i></span>Figma</li>
+						<li><span class="fa-li"><i class="devicon-socketio-original"></i></span>Websocket & SocketIO</li>
+						<li><span class="fa-li"><i class="devicon-android-plain"></i></span>Google Play Console</li>
+						<li><span class="fa-li"><i class="devicon-apple-plain"></i></span>Apple App Store Connect</li>
+						<li><span class="fa-li"><i class="devicon-jira-plain"></i></span>Jira</li>
+						<li><span class="fa-li"><i class="devicon-trello-plain"></i></span>Trello</li>
+						<li><span class="fa-li"><i class="devicon-npm-original-wordmark"></i></span>npm</li>
+						<li><span class="fa-li"><i class="fas fa-check"></i></span>Cross Platform Development</li>
+						<li><span class="fa-li"><i class="fas fa-check"></i></span>Documentation (Software Design Document & Software Requirements Specification)</li>
+						<li><span class="fa-li"><i class="fas fa-check"></i></span>Human Computer Interaction</li>
+
+					</ul>
+					<!-- <div class="subheading mb-3">Workflow</div>
 					<ul class="fa-ul mb-0">
 						<li>
 							<span class="fa-li"><i class="fas fa-check"></i></span>
@@ -95,53 +213,17 @@
 							<span class="fa-li"><i class="fas fa-check"></i></span>
 							Agile Development & Scrum
 						</li>
-					</ul>
-				</div>
-			</section>
-			<hr class="m-0" />
-			<!-- Experience-->
-			<section class="resume-section" id="experience">
-				<div class="resume-section-content">
-					<h2 class="mb-5">Experience</h2>
-					<div class="d-flex flex-column flex-md-row justify-content-between mb-5">
-						<div class="flex-grow-1">
-							<h3 class="mb-0">Senior Web Developer</h3>
-							<div class="subheading mb-3">Intelitec Solutions</div>
-							<p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
-						</div>
-						<div class="flex-shrink-0"><span class="text-primary">March 2013 - Present</span></div>
-					</div>
-					<div class="d-flex flex-column flex-md-row justify-content-between mb-5">
-						<div class="flex-grow-1">
-							<h3 class="mb-0">Web Developer</h3>
-							<div class="subheading mb-3">Intelitec Solutions</div>
-							<p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
-						</div>
-						<div class="flex-shrink-0"><span class="text-primary">December 2011 - March 2013</span></div>
-					</div>
-					<div class="d-flex flex-column flex-md-row justify-content-between mb-5">
-						<div class="flex-grow-1">
-							<h3 class="mb-0">Junior Web Designer</h3>
-							<div class="subheading mb-3">Shout! Media Productions</div>
-							<p>Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.</p>
-						</div>
-						<div class="flex-shrink-0"><span class="text-primary">July 2010 - December 2011</span></div>
-					</div>
-					<div class="d-flex flex-column flex-md-row justify-content-between">
-						<div class="flex-grow-1">
-							<h3 class="mb-0">Web Design Intern</h3>
-							<div class="subheading mb-3">Shout! Media Productions</div>
-							<p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
-						</div>
-						<div class="flex-shrink-0"><span class="text-primary">September 2008 - June 2010</span></div>
-					</div>
+					</ul> -->
 				</div>
 			</section>
 			<hr class="m-0" />
 			<!-- Projects-->
 			<section class="resume-section" id="projects">
 				<div class="resume-section-content">
-					<h2 class="mb-5">Experience</h2>
+					<h2 class="mb-5">Projects</h2>
+					<!-- Trackboard -->
+					<!-- Leealgo -->
+					<!-- Leepath -->
 					<div class="d-flex flex-column flex-md-row justify-content-between mb-5">
 						<div class="flex-grow-1">
 							<h3 class="mb-0">Senior Web Developer</h3>
@@ -159,20 +241,24 @@
 					<h2 class="mb-5">Education</h2>
 					<div class="d-flex flex-column flex-md-row justify-content-between mb-5">
 						<div class="flex-grow-1">
-							<h3 class="mb-0">University of Colorado Boulder</h3>
-							<div class="subheading mb-3">Bachelor of Science</div>
-							<div>Computer Science - Web Development Track</div>
-							<p>GPA: 3.23</p>
+							<h3 class="mb-0 flex-shrink-0">Bachelor of Computer Science</h3>
+							<div class="subheading mb-3 text-primary">Degree from Stony Brook University - GPA: 3.6</div>
+							<div class="flex-shrink-0"><i class="fas fa-calendar-alt"></i>&nbsp;2016 March - 2020 December</div>
+						
+							<p>
+								Notable Project: Trackboard <a href="https://trackboard-d41a2.web.app/index.html">Hosted</a> (Not maintained)
+								<br>
+								My role involved UI/UX prototyping and design, Human Computer Interaction implementation, surveying potential users, documentation, and preparing presentations. 
+								<br>
+								Technologies used: Git, Figma, Firebase, noSql, HTML, Css,
+								<br>
+								Notable Project: Merchandising Society <a href="https://merchandising-society.web.app/">Hosted</a> (Not maintained)
+								<br>
+								Built the website for the Fashion Institute of Technology’s Merchandising Society.
+								My role involved design, documentation, full-stack development, and presentations. 
+								Technologies used: Git, Figma, React, Django, Firebase, Heroku, EC2, S3, RESTful API
+							</p>
 						</div>
-						<div class="flex-shrink-0"><span class="text-primary">August 2006 - May 2010</span></div>
-					</div>
-					<div class="d-flex flex-column flex-md-row justify-content-between">
-						<div class="flex-grow-1">
-							<h3 class="mb-0">James Buchanan High School</h3>
-							<div class="subheading mb-3">Technology Magnet Program</div>
-							<p>GPA: 3.56</p>
-						</div>
-						<div class="flex-shrink-0"><span class="text-primary">August 2002 - May 2006</span></div>
 					</div>
 				</div>
 			</section>
@@ -181,8 +267,8 @@
 			<section class="resume-section" id="interests">
 				<div class="resume-section-content">
 					<h2 class="mb-5">Interests</h2>
-					<p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
-					<p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
+					<!-- <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
+					<p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p> -->
 				</div>
 			</section>
 			<hr class="m-0" />
@@ -239,6 +325,6 @@
     <!-- Third party plugin JS-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
     <!-- Core theme JS-->
-    <script src="js/scripts.js"></script>
+    <script src="scripts/scripts.js"></script>
   </body>
 </html>
